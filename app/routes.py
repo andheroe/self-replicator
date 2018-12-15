d8f128dd070338a6288044a8d92c7eca41565428
Christian Alonzo <alonzo.christ@gmail.com>
2018-12-14 20:49:05 -0500

    First commit

---
+++ app/routes.py
@@ -0,0 +1,41 @@
+from flask import render_template, redirect, url_for, json
+from app import app
+from werkzeug.contrib.fixers import ProxyFix
+from flask_dance.contrib.github import make_github_blueprint, github
+import git
+from git import Repo
+from config import basedir
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+blueprint = make_github_blueprint(
+    app.config['GITHUB_CLIENT_ID'],
+    app.config['GITHUB_CLIENT_SECRET'],
+    scope=['public_repo']
+)
+
+app.register_blueprint(blueprint, url_prefix="/login")
+
+@app.route("/")
+@app.route("/index")
+def index():
+    if not github.authorized:
+        return redirect(url_for("github.login"))
+    origin_repo = github.get('/repos/' + app.config['ORIGIN_REPO_USER'] + '/' + app.config['ORIGIN_REPO_NAME'])
+    origin_repo_name = origin_repo.json()['name']
+    user_login = github.get('/user').json()['login']
+
+    new_repo_name = origin_repo_name
+    duplicate_incrementor = 1
+    while github.get("/repos/" + user_login + '/' + new_repo_name).ok:
+        new_repo_name = origin_repo_name + str(duplicate_incrementor)
+        duplicate_incrementor += 1
+
+    payload = {'name': new_repo_name, 'description': origin_repo.json()['description']}
+    response = github.post("/user/repos", json.dumps(payload))
+
+    repo = Repo.clone_from(origin_repo.json()['clone_url'], basedir + new_repo_name)
+    remote = repo.create_remote('target',response.json()['clone_url'])
+    remote.push(refspec='{}:{}'.format('master','master'))
+
+    return redirect(response.json()['svn_url'])
---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    GITHUB_CLIENT_ID = os.environ.get('GITHUB_CLIENT_ID')
+    GITHUB_CLIENT_SECRET = os.environ.get('GITHUB_CLIENT_SECRET')
+    ORIGIN_REPO_NAME =  os.environ.get('ORIGIN_REPO_NAME')
+    ORIGIN_REPO_USER =  os.environ.get('ORIGIN_REPO_USER')
